8dd5f89e21155dd77b0fc13d177180625e79b4b2
HarishaGB <harshagb555@gmail.com>
2024-01-18 23:50:55 +0530

    hello-world

--- src/index.js
+++ src/index.js
@@ -1,17 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./style.css"
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// JSX (99.99% HTML + 20% jS)
+const Hello=()=>{
+    let a = 555
+    // using object we give css properties 
+    let h1Style = {textAline:"center", border:"5px solid green"}
+    
+    //this part is HTML code
+    return (
+        //its for only single compenent
+        // <h1> Hello World </h1>
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+        //we use multiple elements inside the div exapmle
+        // its external css
+        <div className="hello"> 
+            {/* inline css */}
+            {/* <h1 style={{textAline:"center", border:"5px solid green"}}> Creator is Harisha G B</h1> */}
+            <h1 style={h1Style}> Creator is Harisha G B</h1>
+            <p> {5+5+5} </p>
+            <p> Welcome to World, {a}</p>
+        </div>
+    )
+}
+
+ReactDOM.render(<Hello/>, document.getElementById("root"))
